6aedc1f916253152a2a59b3a1607d186cec9357a
hbh <578092215@qq.com>
2023-03-16 10:59:12 +0800

    feat(filter & middleware): 新增全局错误过滤，新增全局格式化中间件

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,6 +2,8 @@ import { App, Configuration, ILifeCycle } from '@midwayjs/core';
 import { Application } from 'egg';
 import { join } from 'path';
 import * as egg from '@midwayjs/web';
+import { FormatMiddleware } from './middleware/format.middleware'; // 引入自定义中间件
+import { AllErrorFilter } from './filter/base.exception.filter'; // 引入自定义异常过滤器
 import * as orm from '@midwayjs/typeorm'; // 引入typeorm插件
 import * as swagger from '@midwayjs/swagger'; // 引入swagger插件
 
@@ -13,5 +15,8 @@ export class ContainerLifeCycle implements ILifeCycle {
   @App()
   app: Application;
 
-  async onReady() {}
+  async onReady() {
+    this.app.useMiddleware([FormatMiddleware]);
+    this.app.useFilter([AllErrorFilter]);
+  }
 }
